4838f4ea9f68e1542e23ada4bf903eacf37c1533
Riley Hedges <rhedges1@usf.edu>
2025-01-24 18:42:58 -0500

    Task 1 - Store Inventory

--- cc_2.js
+++ cc_2.js
@@ -1,3 +1,14 @@
+/////////////////////////////////
+// Task 1: Working With Arrays //
+/////////////////////////////////
 
+let products = ["Coke", "Pepsi", "Fanta", "Sprite", "Ginger Ale"];
+console.log("Initial Array", products);
+
+products.push("Mug Rootbeer");
+console.log("Added Mug", products);
+
+products.pop();
+console.log("Removed Mug", products);
 
 
