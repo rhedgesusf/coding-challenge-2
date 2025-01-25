7b490048dc013066503143a0cfb84d69f4ec113b
Riley Hedges <rhedges1@usf.edu>
2025-01-24 19:04:23 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -11,4 +11,22 @@ console.log("Added Mug", products);
 products.pop();
 console.log("Removed Mug", products);
 
+////////////////////////////////////////////
+// Task 2: Accessing and Modifying Arrays //
+////////////////////////////////////////////
+
+let scores = [90, 98, 45, 76, 37];
+console.log("Initial Scores", scores);
+
+scores[1] = 99;
+console.log("Updated Second Score", scores);
+
+let sum = 0;
+for (let i = 0; i < scores.length; i++) {
+    sum += scores[i];
+}
+console.log("Sum:", sum);
+
+console.log("Average:", sum / scores.length);
+
 
